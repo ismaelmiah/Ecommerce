b2f45b4195adc23cce77b5571c4d886da44f106d
ismail5g <ismail27.dec@gmail.com>
2020-03-12 23:59:47 +0600

    AddProductTypeTableIntoDatabase

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Online_Shop.Models;
 
 namespace Online_Shop.Data
 {
@@ -12,5 +13,6 @@ namespace Online_Shop.Data
             : base(options)
         {
         }
+        public DbSet<ProductTypes> ProductType  { get; set; }
     }
 }
