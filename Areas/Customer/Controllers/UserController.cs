9cff4c4a59bfa12f9e4774559e5db53e752aa3bb
ismail5g <ismail27.dec@gmail.com>
2020-03-28 16:42:12 +0600

    Registration Form Validated from Backend and Frontend

--- Areas/Customer/Controllers/UserController.cs
+++ Areas/Customer/Controllers/UserController.cs
@@ -2,21 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Online_Shop.Models;
 
 namespace Online_Shop.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class UserController : Controller
     {
+        UserManager<IdentityUser> _userManager;
+        public UserController(UserManager<IdentityUser>userManager)
+        {
+            _userManager = userManager;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpPost]
+        [HttpGet]
         public async Task<IActionResult> Create()
         {
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Create(ApplicationUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userManager.CreateAsync(user, user.PasswordHash);
+                if (result.Succeeded)
+                {
+                    TempData["save"] = "User has been Registered Successfully";
+                    return RedirectToAction(nameof(Index));
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+            }
             return View();
         }
     }
---
+++ wwwroot/js/RegistrationValidation.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $("#basic-form").validate({
+        rules: {
+            FirstName: {
+                required: true  
+            },
+            LastName: {
+                required: true
+            },
+            UserName: {
+                required: true
+            },
+            PasswordHash: {
+                required: true
+            }
+        }
+    });
+});
