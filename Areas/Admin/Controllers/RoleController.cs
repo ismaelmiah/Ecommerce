ce5ac9286f841014748cda0782b1637544a45a74
ismail5g <ismail27.dec@gmail.com>
2020-04-02 21:56:20 +0600

    Role Assigned for users,  Make Role For user,  CRUD for Role

--- Areas/Admin/Controllers/RoleController.cs
+++ Areas/Admin/Controllers/RoleController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Online_Shop.Areas.Admin.Models;
+using Online_Shop.Data;
 
 namespace Online_Shop.Areas.Admin.Controllers
 {
@@ -11,9 +14,13 @@ namespace Online_Shop.Areas.Admin.Controllers
     public class RoleController : Controller
     {
         RoleManager<IdentityRole> _roleManager;
-        public RoleController(RoleManager<IdentityRole>roleManager)
+        UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _db;
+        public RoleController(RoleManager<IdentityRole>roleManager, ApplicationDbContext db, UserManager<IdentityUser>userManager)
         {
             _roleManager = roleManager;
+            _db = db;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -44,6 +51,37 @@ namespace Online_Shop.Areas.Admin.Controllers
             if (result.Succeeded)
             {
                 TempData["save"] = "Role has been saved successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if(role == null)
+            {
+                return NotFound();
+            }
+            ViewBag.id = role.Id;
+            ViewBag.name = role.Name;
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+            var result = await _roleManager.DeleteAsync(role);
+            if (result.Succeeded)
+            {
+                TempData["save"] = "Delete";
                 return RedirectToAction(nameof(Index));
             }
             return View();
@@ -87,5 +125,50 @@ namespace Online_Shop.Areas.Admin.Controllers
             return View();
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Assign()
+        {
+            ViewData["UserId"] = new SelectList(_db.ApplicationUsers.Where(x=>x.LockoutEnd<DateTime.Now || x.LockoutEnd==null).ToList(), "Id", "UserName");
+            ViewData["RoleId"] = new SelectList(_roleManager.Roles.ToList(), "Name", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Assign(RoleUserVm roleUser)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(x=>x.Id==roleUser.userId);
+            var isExist = await _userManager.IsInRoleAsync(user, roleUser.roleId);
+            if (isExist)
+            {
+                ViewBag.msg = "This Role is already assigned for this User";
+                ViewData["UserId"] = new SelectList(_db.ApplicationUsers.Where(x => x.LockoutEnd < DateTime.Now || x.LockoutEnd == null).ToList(), "Id", "UserName");
+                ViewData["RoleId"] = new SelectList(_roleManager.Roles.ToList(), "Name", "Name");
+
+                return View();
+            }
+            var role = await _userManager.AddToRoleAsync(user, roleUser.roleId);
+            if (role.Succeeded)
+            {
+                TempData["save"] = "Role has been Assigned";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        public IActionResult AssignUserRole()
+        {
+            var result = from ur in _db.UserRoles
+                         join r in _db.Roles on ur.RoleId equals r.Id
+                         join a in _db.ApplicationUsers on ur.UserId equals a.Id
+                         select new UserRoleMapping()
+                         {
+                             UserId = ur.UserId,
+                             RoleId = ur.RoleId,
+                             UserName = a.UserName,
+                             RoleName = r.Name
+                         };
+            ViewBag.UserRoles = result;
+            return View();
+        }
     }
 }
---
+++ Areas/Admin/Models/RoleUserVm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_Shop.Areas.Admin.Models
+{
+    public class RoleUserVm
+    {
+        [Required]
+        [Display(Name = "User")]
+        public string userId { get; set; }
+        [Required]
+        [Display(Name = "Role")]
+        public string roleId { get; set; }
+    }
+}
